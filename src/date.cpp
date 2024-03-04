bd93b948e0425f4a972b8e3ce6adaeff07cf406a
Samuel Huang <samhuang91@gmail.com>
2024-03-04 00:01:01 -0800

    Make overload operator more verbose

--- src/date.cpp
+++ src/date.cpp
@@ -5,5 +5,11 @@
 Date::Date(int year, int month, int day) : year(year), month(month), day(day) {}
 
 bool operator<(Date const & lhs, Date const & rhs) {
-  return lhs.getYear() < rhs.getYear() || lhs.getMonth() < rhs.getMonth() || lhs.getDay() < rhs.getDay();
+  if (lhs.getYear() < rhs.getYear()) {
+    return true;
+  } else if (lhs.getMonth() < rhs.getMonth()) {
+    return true;
+  } else {
+    return lhs.getDay() < rhs.getDay();
+  }
 }
