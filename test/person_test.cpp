5a9caee9ff98730214fc92a1909db6902ccff951
Samuel Huang <samhuang91@gmail.com>
2024-03-03 23:33:25 -0800

    Apply formatting

--- test/person_test.cpp
+++ test/person_test.cpp
@@ -4,19 +4,11 @@
 
 class PersonTest : public testing::Test {
  protected:
-  void SetUp() override {
-    person = new Person("John", 28);
-  }
-  void TearDown() override {
-    delete person;
-  }
+  void SetUp() override { person = new Person("John", 28); }
+  void TearDown() override { delete person; }
   Person *person;
 };
 
-TEST_F(PersonTest, NameGetter) {
-  EXPECT_EQ(person->getName(), "John");
-}
+TEST_F(PersonTest, NameGetter) { EXPECT_EQ(person->getName(), "John"); }
 
-TEST_F(PersonTest, AgeGetter) {
-  EXPECT_EQ(person->getAge(), 28);
-}
+TEST_F(PersonTest, AgeGetter) { EXPECT_EQ(person->getAge(), 28); }
