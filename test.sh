d43863d0c0821d581893e65b2fe000a6368873bb
Samuel Huang <samhuang91@gmail.com>
2024-03-03 22:23:59 -0800

    Add comments and error cheking in test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -eux
 
+if [ ! -f build/test ]; then
+    echo -n "Cannot find tests. Please run build.sh first"
+    exit 1
+fi
+
+# Run tests after E2E build
 ctest --test-dir build/test --output-on-failure
