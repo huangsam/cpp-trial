475c781ef7809a80b4e79dcd70d16acdcc223d3c
Samuel Huang <samhuang91@gmail.com>
2024-03-03 16:52:22 -0800

    Add another factorial test

--- test/factorial_test.cpp
+++ test/factorial_test.cpp
@@ -2,10 +2,14 @@
 
 #include <gtest/gtest.h>
 
-TEST(FactorialTest, BasicAssertions) {
+TEST(FactorialTest, TextbookStyle) {
   EXPECT_EQ(factorial(0), 1);
   EXPECT_EQ(factorial(1), 1);
   EXPECT_EQ(factorial(2), 2);
   EXPECT_EQ(factorial(3), 6);
   EXPECT_EQ(factorial(4), 24);
+}
+
+TEST(FactorialTest, OutOfBounds) {
+  EXPECT_EQ(factorial(-1), 1);
 }
