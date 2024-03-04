d8723cfd41c3c1110e5b868939599ff55055f03f
Samuel Huang <samhuang91@gmail.com>
2024-03-04 09:05:13 -0800

    Adjust date tests slightly

--- test/date_test.cpp
+++ test/date_test.cpp
@@ -2,19 +2,25 @@
 
 #include <gtest/gtest.h>
 
-TEST(DateTest, EqualityComparisons) {
+class DateTest : public testing::Test {
+ protected:
   Date one_date = Date(2024, 2, 1);
+};
+
+TEST_F(DateTest, EqualComparisons) {
   ASSERT_EQ(one_date, one_date);
   ASSERT_LE(one_date, one_date);
   ASSERT_GE(one_date, one_date);
 }
 
-TEST(DateTest, InequalityComparisons) {
-  Date one_date = Date(2024, 2, 1);
+TEST_F(DateTest, LesserComparisons) {
   Date newer_date = Date(2024, 2, 2);
-  Date older_date = Date(2024, 1, 31);
   ASSERT_LT(one_date, newer_date);
   ASSERT_LE(one_date, newer_date);
+}
+
+TEST_F(DateTest, GreaterComparisons) {
+  Date older_date = Date(2024, 1, 31);
   ASSERT_GT(one_date, older_date);
   ASSERT_GE(one_date, older_date);
 }
--- test/person_test.cpp
+++ test/person_test.cpp
@@ -4,11 +4,9 @@
 
 class PersonTest : public testing::Test {
  protected:
-  void SetUp() override { person = new Person("John", 28); }
-  void TearDown() override { delete person; }
-  Person *person;
+  Person person = Person("John", 28);
 };
 
-TEST_F(PersonTest, NameGetter) { EXPECT_EQ(person->getName(), "John"); }
+TEST_F(PersonTest, NameGetter) { EXPECT_EQ(person.getName(), "John"); }
 
-TEST_F(PersonTest, AgeGetter) { EXPECT_EQ(person->getAge(), 28); }
+TEST_F(PersonTest, AgeGetter) { EXPECT_EQ(person.getAge(), 28); }
