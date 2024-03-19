0aafc2721611f7a7a49fb7cd0b034675aa30a998
Samuel Huang <samhuang91@gmail.com>
2024-03-19 10:08:19 -0700

    Make type hinting more explicit

--- src/lambda.cpp
+++ src/lambda.cpp
@@ -4,11 +4,11 @@
 #include <vector>
 
 bool lessThan(int a, int b) {
-  auto less_than_lambda = [](auto a, auto b) { return a < b; };
-  return less_than_lambda(a, b);
+  auto less_than = [](int a, int b) { return a < b; };
+  return less_than(a, b);
 }
 
 int sumOfSquares(std::vector<int> numbers) {
-  auto square = [](auto acc, auto i) { return acc + (i * i); };
+  auto square = [](int acc, int i) { return acc + (i * i); };
   return std::reduce(numbers.begin(), numbers.end(), 0, square);
 }
