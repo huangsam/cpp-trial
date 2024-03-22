0670a30e801254b93035c36afc31f031887be3ee
Samuel Huang <samhuang91@gmail.com>
2024-03-22 05:51:04 -0700

    Add clarification on std::reduce

--- src/lambda.cpp
+++ src/lambda.cpp
@@ -10,5 +10,14 @@ bool lessThan(int a, int b) {
 
 int sumOfSquares(std::vector<int> numbers) {
   auto square = [](int acc, int i) { return acc + (i * i); };
+
+  /**
+   * std::reduce works as expected, given that associativity and
+   * commutativity holds for addition and multiplication.
+   * Otherwise, we would use std::accumulate to ensure that we
+   * get a consistent output, albeit with less performance.
+   *
+   * https://stackoverflow.com/q/47144083
+   */
   return std::reduce(numbers.begin(), numbers.end(), 0, square);
 }
