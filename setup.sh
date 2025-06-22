6decc69718d31ea5be0df7eae6139439afbe4463
Samuel Huang <samhuang91@gmail.com>
2025-06-22 03:12:19 -0700

    Remove setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-
-# Install required software:
-# https://formulae.brew.sh/formula/clang-format
-# https://formulae.brew.sh/formula/cmake
-# https://formulae.brew.sh/formula/cppcheck
-# https://formulae.brew.sh/formula/ninja
-brew install --formula clang-format cmake cppcheck ninja
