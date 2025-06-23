c13e65d9edd34d57043b69700ca283d4d88a3677
Samuel Huang <samhuang91@gmail.com>
2025-06-22 21:12:34 -0700

    Use modern std::format for printing

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <iomanip>
+#include <format>
 #include <iostream>
 #include <vector>
 
@@ -7,49 +7,47 @@
 #include "lambda.h"
 #include "person.h"
 
-void print_date(const Date& d) {
-  std::cout << d.get_year() << "/" << d.get_month() << "/" << d.get_day()
-            << std::endl;
-}
+// Specialization for Date
+template <>
+struct std::formatter<Date> : std::formatter<std::string> {
+  static auto format(const Date& d, std::format_context& ctx) {
+    return std::format_to(ctx.out(), "{}/{}/{}", d.get_year(), d.get_month(),
+                          d.get_day());
+  }
+};
 
-void print_person(const Person& p) {
-  // Keep original output settings
-  const std::ios_base::fmtflags originalFlags = std::cout.flags();
-  const std::streamsize originalPrecision = std::cout.precision();
-
-  std::cout << p.get_name() << " is " << p.get_age() << " years old with $";
-
-  std::cout << std::fixed << std::setprecision(2) << std::showpoint
-            << p.get_salary();
-
-  std::cout << " as a salary" << std::endl;
-
-  // Restore original output settings
-  std::cout.flags(originalFlags);
-  std::cout.precision(originalPrecision);
-}
+// Specialization for Person
+template <>
+struct std::formatter<Person> : std::formatter<std::string> {
+  static auto format(const Person& p, std::format_context& ctx) {
+    // Using a format specifier for floating-point numbers (:.2f)
+    return std::format_to(ctx.out(),
+                          "{} is {} years old with ${:.2f} as a salary",
+                          p.get_name(), p.get_age(), p.get_salary());
+  }
+};
 
 int main() {
   // date.h demo
   const auto d1 = Date(2024, 1, 1);
-  print_date(d1);
+  std::cout << std::format("{}\n", d1);
   const auto d2 = Date(2024, 2, 2);
-  print_date(d2);
+  std::cout << std::format("{}\n", d2);
   if (d1 < d2) {
-    std::cout << "First date is older than second date" << std::endl;
+    std::cout << std::format("First date is older than second date\n");
   }
 
   // factorial.h demo
-  std::cout << "factorial(4): " << factorial(4) << std::endl;
+  std::cout << std::format("factorial(4): {}\n", factorial(4));
 
   // lambda.h demo
-  std::cout << "less_than(3,4): " << less_than(3, 4) << std::endl;
-  std::cout << "sum_of_squares(vector{1,2,3}): "
-            << sum_of_squares(std::vector{1, 2, 3}) << std::endl;
+  std::cout << std::format("less_than(3,4): {}\n", less_than(3, 4));
+  std::cout << std::format("sum_of_squares(vector{{1,2,3}}): {}\n",
+                           sum_of_squares(std::vector{1, 2, 3}));
 
   // person.h demo
   const auto p1 = Person("David", 21, 65000.00);
-  print_person(p1);
+  std::cout << std::format("{}\n", p1);
 
   return 0;
 }
