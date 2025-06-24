5d21df5376edf1927ec876ac3b5fb2e5a20cc5a4
Samuel Huang <samhuang91@gmail.com>
2025-06-23 21:25:22 -0700

    Adjust delay of the async greet

--- src/async_simple.cpp
+++ src/async_simple.cpp
@@ -6,6 +6,6 @@
 std::string greet() {
   // Simulate some work being done in the background
   std::this_thread::sleep_for(
-      std::chrono::milliseconds(2000));  // Sleep for 2 seconds
+      std::chrono::milliseconds(100));  // Sleep for 100 milliseconds
   return "Hello, World! from std::async";
 }
